95a45593e9189b76a10e66a092d6aac0984627c3
estellar_boyz <tayalone@gmail.com>
2020-06-25 22:23:39 +0700

    :sparkles: git add .
    git commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-import express from 'express'
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const app = express()
+const app: Application = express()
 
+app.get('/', (req: Request, res: Response) => {
+  return res.send({ message: 'OK' })
+})
+
+// tslint:disable-next-line: no-console
 app.listen(5000, () => console.log('Server running @5000'))
