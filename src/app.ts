db5cf7fb42d3b84d7792753fd6172e1c3a3a9bbf
estellar_boyz <tayalone@gmail.com>
2020-06-25 22:26:36 +0700

    :building_construction: Split Express App to server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express, { Application, Request, Response } from 'express'
+// import express, { Application, Request, Response } from 'express'
 
-const app: Application = express()
+// const app: Application = express()
 
-app.get('/', (req: Request, res: Response) => {
-  return res.send({ message: 'OK' })
-})
+// app.get('/', (req: Request, res: Response) => {
+//   return res.send({ message: 'OK' })
+// })
+
+// // tslint:disable-next-line: no-console
+
+import server from './server'
 
 // tslint:disable-next-line: no-console
-app.listen(5000, () => console.log('Server running @5000'))
+server.listen(5000, () => console.log('Server running @5000'))
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send({ message: 'OK' })
+})
+
+// // tslint:disable-next-line: no-console
+// app.listen(5000, () => console.log('Server running @5000'))
+
+export default app
